34760f601603b6310f52930ada07c194f9b805c6
Asritha123 <asrithakondapaneni@gmail.com>
2021-07-06 23:49:36 +0530

    Calculator Component Using Services

---
+++ AngularAssignment/Assignment9/src/app/calculator/calculator.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CalculatorService } from '../services/calculator.service';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  number1:any
+  number2:any
+  result:any
+
+
+  constructor(private service:CalculatorService) { }
+
+  ngOnInit(): void {
+  }
+  Perform(operator:any,group:any)
+  {
+    var a =(Number)(this.number1)
+    var b =(Number)(this.number2)
+    this.result= this.service.evaluate(a,b,operator,group)
+
+  }
+}
